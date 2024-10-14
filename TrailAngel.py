28dccf35b733c6ca0e55b573bf101cc8b58e3b36
Johnny <jhume001@gmail.com>
2024-10-13 22:06:12 -0400

    Adding everything here to take main branch name

---
+++ TrailAngel.py
@@ -0,0 +1,9 @@
+class TrailAngel:
+
+    def __init__(self, id, first_name: str, last_name: str, location: str, capacity: int, cost: int) -> None:
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.location = location
+        self.capacity = capacity
+        self.cost = cost
