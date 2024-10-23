afac7ccf8c04f897bae1e6a964303ad5c988fb68
Johnny <jhume001@gmail.com>
2024-10-23 17:36:01 -0400

    Add dynamic object to save row method

--- data/database.py
+++ data/database.py
@@ -7,40 +7,27 @@ class Database:
         self.db_name = database_name
         self.trail_angels_table = "trailangels"
         self.hikers_table = "hikers"
+        self.posts_table = "posts"
 
     def setup(self):
         self.__create_tables()
+
+    # ===== POSTS =====
+    def save_post(self, post):
+        row = self.__save_row(self.posts_table, post)
+        return row
 
     # ===== HIKERS =====
     def get_hikers(self):
         return self.__fetch_all(self.hikers_table)
 
     def save_hiker(self, hiker):
-
-        values = (
-            hiker.__dict__['trail_name'],
-            hiker.__dict__['bio']
-        )
-
-        insert_sql = f"""INSERT INTO {self.hikers_table}(trail_name,bio) VALUES(?,?) RETURNING *"""
-        row = self.__execute_with_values(insert_sql, values)
-        return row
+        return self.__save_row(self.hikers_table, hiker)
 
     # ===== TRAIL ANGELS =====
 
-    def save_trail_angel(self, angel):
-
-        values = (
-            angel.__dict__['first_name'],
-            angel.__dict__['last_name'],
-            angel.__dict__['location'],
-            angel.__dict__['capacity'],
-            angel.__dict__['cost']
-        )
-
-        insert_sql = f"""INSERT INTO {self.trail_angels_table}(first_name,last_name,location,capacity,cost) VALUES(?,?,?,?,?) RETURNING *"""
-        row = self.__execute_with_values(insert_sql, values)
-        return row
+    def save_trail_angel(self, trail_angel):
+        return self.__save_row(self.trail_angels_table, trail_angel)
 
     def get_trail_angels(self):
         return self.__fetch_all(self.trail_angels_table)
@@ -51,7 +38,7 @@ class Database:
         return cursor.fetchone()
 
     def __create_tables(self):
-        trail_angel_sql = f"""CREATE TABLE IF NOT EXISTS {self.trail_angels_table}(
+        trail_angels_sql = f"""CREATE TABLE IF NOT EXISTS {self.trail_angels_table}(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             first_name TEXT NOT NULL,
             last_name TEXT NOT NULL,
@@ -61,15 +48,50 @@ class Database:
         )
         """
 
-        hiker_sql = f"""CREATE TABLE IF NOT EXISTS {self.hikers_table}(
+        hikers_sql = f"""CREATE TABLE IF NOT EXISTS {self.hikers_table}(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             trail_name TEXT NOT NULL,
             bio TEXT NOT NULL
         )
         """
 
-        self.__execute(trail_angel_sql)
-        self.__execute(hiker_sql)
+        posts_sql = f"""CREATE TABLE IF NOT EXISTS {self.posts_table}(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            hiker_id INTEGER NOT NULL,
+            title TEXT NOT NULL,
+            pickup TEXT NOT NULL,
+            dropoff TEXT NOT NULL,
+            date TEXT NOT NULL,
+            num_passengers INTEGER,
+            FOREIGN KEY(hiker_id) REFERENCES hikers(id)
+        )
+        """
+
+        self.__execute(trail_angels_sql)
+        self.__execute(hikers_sql)
+        self.__execute(posts_sql)
+
+    def __save_row(self, table_name, obj):
+
+        items = obj.__dict__.items()
+
+        values = ()
+        values_holders = ""
+        keys_str = ""
+        for k, v in items:
+            if (k != "id"):
+                values = values + (v,)
+                keys_str += k + ","
+                values_holders += "?,"
+
+        keys_str = keys_str[:-1]
+        values_holders = values_holders[:-1]
+
+        insert_sql = f"""INSERT INTO {table_name}({keys_str}) VALUES({values_holders}) RETURNING *"""
+        row = self.__execute_with_values(insert_sql, values)
+        return row
+
+
 
     def __fetch_all(self, table: str):
         sql = f"SELECT * FROM {table} WHERE 1"
