1adbc5567870cf0a1a6aec13efd0dca0250ba0e7
Johnny <jhume001@gmail.com>
2024-10-23 17:36:15 -0400

    Add default hiker_id

--- Hiker.py
+++ Hiker.py
@@ -1,6 +1,6 @@
 class Hiker:
 
-    def __init__(self, id, trail_name: str, bio: str) -> None:
+    def __init__(self, trail_name: str, bio: str, id = None) -> None:
         self.id = id
         self.trail_name = trail_name
         self.bio = bio
