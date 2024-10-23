a49e5f993112c15abce8607a63afd34a4437e9f6
Johnny <jhume001@gmail.com>
2024-10-23 17:36:58 -0400

    Add default trail_angel id

--- TrailAngel.py
+++ TrailAngel.py
@@ -1,6 +1,6 @@
 class TrailAngel:
 
-    def __init__(self, id, first_name: str, last_name: str, location: str, capacity: int, cost: int) -> None:
+    def __init__(self, first_name: str, last_name: str, location: str, capacity: int, cost: int, id = None) -> None:
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
