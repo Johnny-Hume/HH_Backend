f0996d8340431034656746033d3b1d30db49c605
Johnny <jhume001@gmail.com>
2024-10-23 20:13:25 -0400

    Add get_posts. Add validation hiker_exists to create_post

--- PostService.py
+++ PostService.py
@@ -1,12 +1,26 @@
 from Post import Post
+from HikerService import HikerService
 class PostService:
 
     def __init__(self, db) -> None:
         self.db = db
 
     def create_post(self, post):
+        hiker = HikerService(self.db).get_hiker(post.hiker_id)
+        if not hiker:
+            raise Exception("Problem validating hiker creating post")
         row = self.db.save_post(post)
         return self.post_from_row(row)
+
+    def get_posts(self): 
+        rows = self.db.get_posts()
+        return self.posts_from_rows(rows)
+
+    def posts_from_rows(self, rows):
+        posts = []
+        for row in rows:
+            posts.append(self.post_from_row(row))
+        return posts
 
     def post_from_row(self, row):
 
