e0f749030a21134723d58479d823e003defb4f6d
Johnny <jhume001@gmail.com>
2024-11-23 21:16:35 +0000

    Refactor move and delete trailangelfactory code

--- service/TrailAngelService.py
+++ service/TrailAngelService.py
@@ -1,24 +1,33 @@
 from werkzeug.exceptions import NotFound
-from TrailAngelFactory import TrailAngelFactory
+from domain.TrailAngel import TrailAngel
 
 class TrailAngelService:
 
     def __init__(self, db) -> None:
-        self.trail_angel_factory = TrailAngelFactory()
         self.db = db
 
     def get_all_trail_angels(self):
         rows = self.db.get_trail_angels()
-        trail_angels = self.trail_angel_factory.trail_angels_from_rows(rows)
+        trail_angels = self.trail_angels_from_rows(rows)
         return trail_angels
 
     def get_trail_angel(self, trail_angel_id):
         row = self.db.get_trail_angel(trail_angel_id)
         if not row:
             raise NotFound(f"Trail Angel Id [{trail_angel_id}] Not Found")
-        trail_angel = TrailAngelFactory().trail_angel_from_row(row)
+        trail_angel = self.trail_angel_from_row(row)
         return trail_angel
 
     def create_trail_angel(self, trail_angel):
         row = self.db.save_trail_angel(trail_angel)
-        return self.trail_angel_factory.trail_angel_from_row(row)
+        return self.trail_angel_from_row(row)
+
+    def trail_angel_from_row(self, row):
+        id = row[0]
+        return TrailAngel(row[1], row[2], row[3], row[4], row[5], id)
+
+    def trail_angels_from_rows(self, rows: list):
+        angels = []
+        for tuple in rows:
+            angels.append(self.trail_angel_from_row(tuple))
+        return angels
